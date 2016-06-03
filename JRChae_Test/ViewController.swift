9991968dab0bf1288591325ddabf7af6724419c3
Jaeryong Chae <jr_chae@naver.com>
2016-06-03 16:36:29 +0900

    Initial Commit

---
+++ JRChae_Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JRChae_Test
+//
+//  Created by MF839-008 on 2016. 6. 3..
+//  Copyright © 2016년 JRChae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
